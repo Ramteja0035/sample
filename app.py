a250ee0af07f5845ac7ae0191721f485ef642ba6
Ramteja0035 <133530362+Ramteja0035@users.noreply.github.com>
2024-02-05 18:09:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 # app.py
 from flask import Flask, render_template
 import file
-# file.py
-
 def my_function():
-    return "Hello, World!"
-
-
-app = Flask(__name__)
-
+    return "Hello, World
+# app.py
 @app.route('/')
 def index():
     # Call the function from file.py
     output = file.my_function()
     return render_template('index.html', output=output)
-
-if __name__ == '__main__':
-    app.run(debug=True)
